ccdc34ceda6a42506f63096739725aa882334048
vinessa23 <vinessa2323@gmail.com>
2022-05-29 18:59:35 +0700

    display transaction (not yet finished)

---
+++ lib/services/display_transactions.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:retreat/models/transactions.dart';
+
+class TransactionService {
+  final client = Supabase.instance.client;
+
+  Future<Transactions> getTransactions(context) async {
+    print('current user: ${client.auth.currentUser?.id}');
+
+    final result = await client
+        .from('transactions')
+        .select()
+        .eq('id', client.auth.currentUser?.id)
+        .execute();
+
+    print('Data: ${result.data.toString()}');
+
+    if (result.error?.message != null) {
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('Error: ${result.error!.message.toString()}'),
+        duration: const Duration(seconds: 2),
+      ));
+    }
+    final dataList = result.data as List;
+
+    print('Data: ${result.data.toString()}');
+
+    return dataList.map((e) => Transactions.fromJson(e)).toList().elementAt(0);
+  }
+}
