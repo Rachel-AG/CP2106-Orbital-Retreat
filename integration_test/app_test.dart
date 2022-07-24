0133bac999990eb8b7c91ddea2c548efe614fe03
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-07-24 20:38:18 +0800

    fixed integration test set up for driver

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -17,7 +17,7 @@ import 'package:retreat/services/transactions_service.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() {
-  final binding = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
   Future<void> initSupabase() async {
     await Supabase.initialize(
