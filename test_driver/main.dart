9534107d19b2d74019429b6ab7ffc8174645cfad
Rachel Angelyn Gunawan <rachelag777@gmail.com>
2022-07-24 21:22:33 +0800

    add driver extension on main.dart for test

--- test_driver/main.dart
+++ test_driver/main.dart
@@ -1,3 +1,7 @@
 import 'package:integration_test/integration_test_driver.dart';
+import "package:flutter_driver/driver_extension.dart";
 
-Future<void> main() => integrationDriver();
+Future<void> main() {
+  enableFlutterDriverExtension();
+  return integrationDriver();
+}
